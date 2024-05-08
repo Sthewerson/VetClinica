ffabe42807150058ae1e70f49f365c2a1d70cb84
Sthewerson <fernando.rocha@im.ufal.br>
2024-05-08 13:28:17 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
