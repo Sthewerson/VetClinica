ff76cbb1d840d11c7d65dfddf866f1e48f2be9c0
Sthewerson <fernando.rocha@im.ufal.br>
2024-05-09 21:34:08 -0300

    feat: add PetService

---
+++ src/services/PetServices.ts
@@ -0,0 +1,58 @@
+import { getRepository } from "typeorm";
+import { Pet } from "../entities/Pet";
+import { Tutor } from "../entities/Tutor";
+export class PetService {
+  private petRepository = getRepository(Pet);
+  private tutorRepository = getRepository(Tutor);
+
+  async getPets(): Promise<Pet[]> {
+    return await this.petRepository.find();
+  }
+
+  async createPet(
+    name: string,
+    species: string,
+    carry: string,
+    weight: number,
+    dateOfBirth: Date,
+    tutorId: number
+  ): Promise<Pet> {
+    const pet = new Pet();
+    pet.name = name;
+    pet.species = species;
+    pet.carry = carry;
+    pet.weight = weight;
+    pet.dateOfBirth = dateOfBirth;
+
+    const tutor = await this.tutorRepository.findOne({
+      where: { id: tutorId },
+    });
+
+    if (!tutor) {
+      throw new Error("Tutor not found");
+    }
+    pet.tutor = tutor;
+    return await this.petRepository.save(pet);
+  }
+
+  async updatePet(
+    id: number,
+    name: string,
+    species: string,
+    carry: string,
+    weight: number,
+    dateOfBirth: Date
+  ): Promise<void> {
+    await this.petRepository.update(id, {
+      name,
+      species,
+      carry,
+      weight,
+      dateOfBirth,
+    });
+  }
+
+  async deletePet(id: number): Promise<void> {
+    await this.petRepository.delete(id);
+  }
+}
